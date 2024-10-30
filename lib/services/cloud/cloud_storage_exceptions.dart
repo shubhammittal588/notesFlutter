fbec67735105b6fecb84f8e9c00f2214961878ba
shubham-TUL-Luxury <smittal_t@tataunistore.com>
2024-10-30 23:44:28 +0530

    Migrating to our Firestore Service

---
+++ lib/services/cloud/cloud_storage_exceptions.dart
@@ -0,0 +1,11 @@
+class CloudStorageException implements Exception {
+  const CloudStorageException();
+}
+
+class CouldNotCreateNoteException implements Exception {}
+
+class CouldNotGetAllNoteException implements Exception {}
+
+class CouldNotUpdateNoteException implements Exception {}
+
+class CouldNotDeleteNoteException implements Exception {}
